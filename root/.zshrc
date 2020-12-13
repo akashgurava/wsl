3c122c671ddd6b5766494b3162a2db470ad183f7
akash <akashgurava@outlook.com>
2020-12-14 04:40:32 +0530

    changed to antibody and starship

--- root/.zshrc
+++ root/.zshrc
@@ -1,24 +1,54 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block, everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+source ~/.zsh_plugins.sh # point to the plugins file
+
+# use with a bundle name to add it to the txt file
+function antibody_new {
+  if ! [[ $# -eq 0 ]]; then
+    printf '%s\n%s\n' "$1" "$(cat ~/.zsh_plugins.txt)" >~/.zsh_plugins.txt
+    echo "added '$1' to ~/.zsh_plugins.txt"
+  fi # use without bundle name to just update your bundle list
+  antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+  antibody update
+}
+
+# Options
+setopt hist_ignore_all_dups # remove older duplicate entries from history
+setopt inc_append_history # save history entries as soon as they are entered
+setopt hist_reduce_blanks # remove superfluous blanks from history items
+setopt share_history # share history between different instances
+setopt always_to_end # move cursor to end if word had one match
+setopt auto_list # automatically list choices on ambiguous completion
+setopt auto_menu # automatically use menu completion
+setopt auto_cd # cd by typing directory name if it's not a command
+
+# Enable autocompletions
+autoload -Uz compinit
+typeset -i updated_at=$(
+  date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null
+)
+if [ $(date +'%j') != $updated_at ]; then
+  compinit -i
+else
+  compinit -C -i
 fi
+zmodload -i zsh/complist
 
-#
-# Executes commands at the start of an interactive session.
-#
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
+# Improve autocompletion style
+zstyle ':completion:*' menu select # select completions with arrow keys
+zstyle ':completion:*' group-name '' # group results by category
+zstyle ':completion:::::' completer _expand _complete _ignored _approximate # approximate matches
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=247' # color fitting my terminal bg, using 256 color code
 
-# Source Prezto.
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+# Save history for auto suggestions
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=100000
+SAVEHIST=$HISTSIZE
+
+# Keybindings
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+if command -v starship &> /dev/null
+then
+  eval $(starship init zsh)
 fi
-
-# Customize to your needs...
-source ~/powerlevel10k/powerlevel10k.zsh-theme
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- root/init.sh
+++ root/init.sh
@@ -1,54 +1,106 @@
-#!/bin/bash
+# #!/bin/bash
+# #
+# # Helper script to facilitate easy re-installing of WSL setup.
 
-init() {
-    sudo apt update
-    sudo apt -y upgrade
-    sudo apt install -y build-essential
+# Acquire Sudo permissions at the beginning of the script
+sudo "true"
+
+# Set Debian Frontend as noninteractive as we do not want to interact
+# during installation
+sudo dpkg-reconfigure debconf --frontend=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
+
+#######################################
+# Helper function to install packages.
+# Arguments:
+#   comma separated packages to install.
+# Returns:
+#   0 if install successful.
+# Outputs:
+#   Error messages if any.
+#######################################
+function install_helper() {
+  sudo apt-get --yes --allow-downgrades install "$@" > /dev/null
 }
 
-prezto(){
-    clear
-    sudo apt-get install -y git
-    sudo apt-get install -y zsh
-    # Get prezto
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-
-    # Backup zsh config if it exists
-    if [ -f ~/.zshrc ];
-       then
-           mv ~/.zshrc ~/.zshrc.backup
-    fi
-
-    # Create links to zsh config files
-    ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
-    ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
-    ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
-    ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
-    ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
-    ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
-
-    if grep -Fxq "exec zsh" ~/.bashrc
-    then
-        echo "ZSH already added to ~/.bashrc"
-    else
-        printf '\nif [ -t 1 ]; then\nexec zsh\nfi' >> ~/.bashrc
-    fi
-
-    # Install powerline10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+#######################################
+# Update Ubuntu system components.
+# Arguments:
+#   None
+#######################################
+function update() {
+  sudo apt-get --yes --allow-downgrades update > /dev/null
+  sudo apt-get --yes --allow-downgrades upgrade > /dev/null
 }
 
-node.install(){
-    # init
-    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#######################################
+# Install Zsh shell.
+# Arguments:
+#   None
+#######################################
+function install_zsh(){
+  (install_helper zsh)
+  
+  sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
+  
+  # Change Default shell to zsh
+  chsh -s $(which zsh)
+}
 
-    # # Restart shell
-    # exec zsh -l
+#######################################
+# Install antibody for Zsh shell and copy config files.
+# Arguments:
+#   None
+#######################################
+function install_antibody(){
+  (install_zsh)
+  
+  echo bash | sudo -S curl -sfL git.io/antibody | sudo -S sh -s - -b /usr/local/bin
+  
+  cp .zsh_plugins.txt ~/.zsh_plugins.txt
+  cp .zshrc ~/.zshrc
+}
 
-    # # Install Node
-    # nvm install stable
+#######################################
+# Install starship and add config for Zsh.
+# Arguments:
+#   None
+#######################################
+function install_starship() {
+  (install_antibody)
+  
+  curl -fsSL https://starship.rs/install.sh | sudo bash -s -- -y
+  
+  mkdir -p ~/.config && cp starship.toml ~/.config/starship.toml
+}
 
-    curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+#######################################
+# Install build-essesntial.
+# Arguments:
+#   None
+#######################################
+install_build(){
+  # Update OS before proceeding
+  (update)
+  
+  (install_helper build-essential)
+}
+
+#######################################
+# Install python.
+# Arguments:
+#   None
+#######################################
+install_python(){
+  # Install Build Essentials
+  (install_build)
+  
+  # Install pre requisites
+  (install_helper software-properties-common)
+  
+  # Install python
+  (install_helper python3.8 python3-pip)
+  
+  # Create symlink for python with python 3.8
+  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 }
